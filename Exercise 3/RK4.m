9c6d3bb2d2b1d341acbf9e42bfdfc421278426e4
EdwardHarriss <harriss.edward24@gmail.com>
2020-03-02 22:30:22 +0000

    All working but not all tested

--- Exercise 3/RK4.m
+++ Exercise 3/RK4.m
@@ -1,20 +1,14 @@
 function [t, qc, qc_grad] = RK4(F, h, t, qc, qc_grad)
     for i=1: (length(t)-1)
         
-        k1 = qc_grad(i);
-        m1 = F(t(i), qc(i), qc_grad(i));
+        k1 = h*F(t(i), qc(i), qc_grad(i));
+        k2 = h*F(t(i), qc(i) + h/2, qc_grad(i) + k1/2);
+        k3 = h*F(t(i), qc(i) + h/2, qc_grad(i) + k2/2);
+        k4 = h*F(t(i), qc(i) + h , qc_grad(i) + k3);
         
-        k2 = qc_grad(i) + (m1*h/2);
-        m2 = F(t(i) + 0.5*h, qc(i) + (0.5*h*k1), qc_grad(i)+(0.5*h*m1));
         
-        k3 = qc_grad(i) + (m2*h);
-        m3 = F(t(i) + 0.5*h, qc(i) + (0.5*h*k2), qc_grad(i)+(0.5*h*m2));
-        
-        k4 = qc_grad(i) + (m3);
-        m4 = F(t(i) + h, qc(i) + (k3*h), qc_grad(i) + (m3*h));
-        
-        qc_grad(i+1) = qc_grad(i) + (k1+2*k2+2*k3+k4)*h/6;
-        qc(i+1) = qc(i) + (m1+2*m2+2*m3+m4)*h/6;
+        qc_grad(i+1) = qc_grad(i) + (k1 + 2*k2 + 2*k3 + k4)/6;
+        qc(i+1) = qc(i) + h*qc_grad(i);
         t(i+1) = t(i) + h;
     end
 end
--- Exercise 3/RLC_Script.m
+++ Exercise 3/RLC_Script.m
@@ -1,34 +1,69 @@
-%Setting up the T domain
-h = 0.1;
-tf = 2;
+R = 250;
+L = 600*10^(-3);
+C = 3.5*10^(-6);
+
+qc0 = 500*10^(-9);
+
+%System properties
+h = 0.00001;
+tf = 0.05;
 t0 = 0;
 N = tf/h;
+
 %Setting up Variables
 qc = zeros(1,N);
-qc(1) = 500*10^(-9);
+qc(1) = qc0;
 qc_grad = zeros(1,N);
 Vout = zeros(1,N);
 t = zeros(1,N);
 t(1) = t0;
-F = @f2;
 
-[t, qc, qc_grad]= RK4(F, h, t, qc, qc_grad);
+F = @y;
+
+%Call to RK4
+[t, qc, qc_grad] = RK4(F, h, t, qc, qc_grad);
 
 Vout = R*qc_grad;
 
+%obtaining plot
 plot(t, Vout);
 xlabel('Time');
 ylabel('Potential Difference');
+hold on;
 
-function [yout] = f2(time, qc, qc_grad)
+
+
+
+%System function. other one doesn't have to be defined (just qc_dash)
+%change number after Vin for different imput signals shown below
+function [yout] = y(t, qc, qc_grad)
     R = 250;
-	C = 3.5*10^(-6);
     L = 600*10^(-3);
-    yout = (1/L)*(Vin(time) - R*qc_grad - (1/C)*qc);
+    C = 3.5*10^(-6);
+    yout = (Vin3(t) - (qc/C) - (R*qc_grad))/L;
 end
 
-function [volt_out] = Vin(time);
+% (1) heaviside Vin
+function [volt_out] = Vin1(time)
     volt_out = 5;
+end
+
+
+% (2) impulsive signal
+function [volt_out] = Vin2(time)
+    volt_out = 5*exp(-(time^2)/3);
+end
+
+% (3) square wave 
+function [volt_out] = Vin3(time)
+    freq = 109;
+    volt_out = 5*square(2*pi*freq*time);
+end
+
+% (4) sine wave
+function [volt_out] = Vin4(time)
+    freq = 109;
+    volt_out = 5*sin(2*pi*freq*time);
 end
 
 
