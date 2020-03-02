3375e00d658c6afbeb8cf70e96bcf9cb1c4c20f8
Ed Harriss <harriss.edward24@gmail.com>
2020-03-02 14:39:32 +0000

    Nothing special, just changed around the function

--- Exercise 3/RLC_Script.m
+++ Exercise 3/RLC_Script.m
@@ -13,31 +13,34 @@ t = zeros(1,N);
 t(1) = t0;
 F1 = @f1;
 F2 = @f2;
-%Input Signal
-Vin = 5;
 
+for i=1: (length(t)-1)
+    t(i+1) = t(i)+h;        %not needed currently for later
+end
 
 [qc, qc_grad]= RK4(F1, F2, h, t, qc, qc_grad);
 
-for i=1: (length(t)-1)
-    t(i+1) = t(i)+h;
-    Vout(i) = R*abs(qc_grad(i));
-end
+Vout = R*qc_grad;
 
 plot(t, Vout);
 set(gca, 'YScale', 'log')
 xlabel('Time');
-ylabel('Amplitude');
+ylabel('Potential Difference');
 
    
-function [yout] = f1(Vin, qc, qc_grad)
+function [yout] = f1(time, qc, qc_grad)
     yout = qc_grad;
 end
 
-function [yout] = f2(Vin, qc, qc_grad)
+function [yout] = f2(time, qc, qc_grad)
     R = 250;
 	C = 3.5*10^(-6);
     L = 600*10^(-3);
-    yout = (1/L)*(Vin - R*qc_grad - (1/C)*qc);
+    yout = (1/L)*(Vin(time) - R*qc_grad - (1/C)*qc);
 end
+
+function [volt_out] = Vin(time);
+    volt_out = 5;
+end
+
 
