aa0b3e92c948de5bb400713ea59c10bd7ec4c9e1
kimongrigorakis <56482922+kimongrigorakis@users.noreply.github.com>
2020-03-03 23:00:19 +0000

    Changes

--- Exercise 1/RK2_script.m
+++ Exercise 1/RK2_script.m
@@ -1,25 +1,48 @@
 t_i = 0; % set initial value of t_0
 q_i = 500e-9; % set q_initial condition q at t_0
-h = 0.0003; % set t step-size
-t_f = 1; % stop here
+h = 0.00001; % set t step-size
+t_f = 0.001; % stop here
 R = 1000; % resistance
 C = 100e-9; % capacitance
 
 
 %******************Step_Signal 2.5V******************
-func = @(t, q) 2.5/R - 1/(R*C)*q; %function handle: 2 variables
+% func = @(t, q) 2.5/R*heaviside(t) - 1/(R*C)*q; %function handle: 2 variables
+%
+% [tout, qout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
+% plot(tout, qout, 'b') %plot now values of x,y
+% 
+% figure; % use this if you want the exact solution and the numerical on different figures
+% %hold on; % use this if you want the exact solution and the numerical on the same figure
+% 
+% qexact = 2.5 * C * ( 1 + exp(-tout/(R*C)) ); %calculate exact solution
+% plot(tout, qexact, 'r') %plot now values of x,y
 %****************************************************
 
 
 % %******************Impulsive_Signal 2.5V******************
 % tau = 100;
 % func = @(t, q) 2.5/R*exp(-t^2/tau) - 1/(R*C)*q; %function handle: 2 variables
+% 
+% [tout, qout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
+% plot(tout, qout, 'b') %plot now values of x,y
+% 
+% There cannot be an exact solution for this Vin
 % %*********************************************************
 
 
 % %******************Decay_Signal 2.5V******************
 % tau = 100;
 % func = @(t, q) 2.5/R*exp(-t/tau) - 1/(R*C)*q; %function handle: 2 variables
+% 
+% [tout, qout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
+% plot(tout, qout, 'b') %plot now values of x,y
+% 
+% figure; % use this if you want the exact solution and the numerical on different figures
+% % %hold on; % use this if you want the exact solution and the numerical on the same figure
+% 
+% qexact = 2.5*tau*C/(tau - R*C)*(exp(-tout/tau) - exp(-tout/(R*C))) + q_i*exp(-tout/(R*C)); %calculate exact solution
+% plot(tout, qexact, 'r') %plot now values of x,y
 % %*****************************************************
 
 
@@ -29,6 +52,15 @@ func = @(t, q) 2.5/R - 1/(R*C)*q; %function handle: 2 variables
 % period = 500e-6;
 % period = 1000e-6;
 % func = @(t, q) 5/R*sin(2*pi/period*t) - 1/(R*C)*q; %function handle: 2 variables
+% 
+% [tout, qout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
+% plot(tout, qout, 'b') %plot now values of x,y
+% 
+% figure; % use this if you want the exact solution and the numerical on different figures
+% % %hold on; % use this if you want the exact solution and the numerical on the same figure
+% 
+% qexact = 2.5*tau*C/(tau - R*C)*(exp(-tout/tau) - exp(-tout/(R*C))) + q_i*exp(-tout/(R*C)); %calculate exact solution
+% plot(tout, qexact, 'r') %plot now values of x,y
 % %*****************************************************
 
 
@@ -38,6 +70,11 @@ func = @(t, q) 2.5/R - 1/(R*C)*q; %function handle: 2 variables
 % period = 500e-6;
 % period = 1000e-6;
 % func = @(t, q) 5/R*square(2*pi/period*t) - 1/(R*C)*q; %function handle: 2 variables
+% 
+% [tout, qout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
+% plot(tout, qout, 'b') %plot now values of x,y
+% 
+% There cannot be an exact solution for this Vin
 % %*****************************************************
 
 
@@ -47,8 +84,9 @@ func = @(t, q) 2.5/R - 1/(R*C)*q; %function handle: 2 variables
 % period = 500e-6;
 % period = 1000e-6;
 % func = @(t, q) 5/R*sawtooth(2*pi/period*t) - 1/(R*C)*q; %function handle: 2 variables
+% 
+% [tout, qout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
+% plot(tout, qout, 'b') %plot now values of x,y
+% 
+% There cannot be an exact solution for this Vin
 % %********************************************************
-
-
-[xout, yout] = RK2(func, t_i, q_i, t_f, h); % call to euler.m
-plot(xout, yout, 'b') %plot now values of x,y
