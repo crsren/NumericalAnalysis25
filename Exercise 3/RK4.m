0b65b776c0fadc2c3df26325da307f7152ab8a1a
Ed Harriss <harriss.edward24@gmail.com>
2020-03-03 15:10:55 +0000

    Final function and basic photos

--- Exercise 3/RK4.m
+++ Exercise 3/RK4.m
@@ -1,14 +1,16 @@
-function [t, qc, qc_grad] = RK4(F, h, t, qc, qc_grad)
-    for i=1: (length(t)-1)
-        
-        k1 = h*F(t(i), qc(i), qc_grad(i));
-        k2 = h*F(t(i), qc(i) + h/2, qc_grad(i) + k1/2);
-        k3 = h*F(t(i), qc(i) + h/2, qc_grad(i) + k2/2);
-        k4 = h*F(t(i), qc(i) + h , qc_grad(i) + k3);
-        
-        
-        qc_grad(i+1) = qc_grad(i) + (k1 + 2*k2 + 2*k3 + k4)/6;
-        qc(i+1) = qc(i) + h*qc_grad(i);
+function [Vout, t, qc, qc_grad] = RK4(Vout, F2, F1, h, t, qc, qc_grad, N)
+    for i=1:N 
+        k1 = F1(t(i), qc(i), qc_grad(i));
+        m1 = F2(t(i), qc(i), qc_grad(i));
+        k2 = F1(t(i) + 0.5*h, qc(i) + (0.5*h*k1), qc_grad(i)+(0.5*h*m1));
+        m2 = F2(t(i) + 0.5*h, qc(i) + (0.5*h*k1), qc_grad(i)+(0.5*h*m1));
+        k3 = F1(t(i) + 0.5*h, qc(i) + (0.5*h*k2), qc_grad(i)+(0.5*h*m2));
+        m3 = F2(t(i) + 0.5*h, qc(i) + (0.5*h*k2), qc_grad(i)+(0.5*h*m2));
+        k4 = F1(t(i) + h, qc(i) + (k3*h), qc_grad(i) + (m3*h));
+        m4 = F2(t(i) + h, qc(i) + (k3*h), qc_grad(i) + (m3*h));
+        qc(i+1) = qc(i) + ((k1+k4)/6 + (k2+k3)/3)*h;
+        qc_grad(i+1) = qc_grad(i) + ((m1+m4)/6 + (m2+m3)/3)*h;
         t(i+1) = t(i) + h;
+        Vout(i+1) = qc_grad(i+1)*250;
     end
-end
+end 
