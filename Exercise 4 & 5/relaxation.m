494644f87d94f3601f93cd9e28e8f79d6658856a
Chris <chris.renschler@gmail.com>
2020-03-06 01:16:01 +0000

    Now works with all well behaved boundary conditions

--- Exercise 4 & 5/relaxation.m
+++ Exercise 4 & 5/relaxation.m
@@ -1,28 +1,29 @@
 % Normal relaxation method
 % later: function [iterations,U] = relaxation(h,n,m,epsilon,Ui_min,Ui_max,Umin_j,Umax_j)
   
-%-> Next: implement functions as boundary conditions
+% [ ] examples for other poisson functions ( = g(x) instead of 0)
+% [ ] understand SOR
 
 close all ; clc
 
 % Initialize grid (all zero)
-h = 1; n = 30; m = 30;
-x = 0:h:n;
-y = 0:h:m;
+h = 0.1; n = 20; m = 20;
+x = 0:h:h*n;
+y = 0:h:h*m;
 U = zeros(n+1,m+1);
 
 % Set boundary conditions
 for i = 1 : length(x)
-    U(i ,1) = 20;
-    U(i,length(y)) = 20;
+    U(i ,1) = (i-11)^2-100;
+    U(i,length(y)) = (i-11)^2-100;
 end
 for j = 1 : length(y)
-    U(1, j) = 10;
-    U(length(x), j) = 0;
+    U(1, j) = -(j-11)^2+100;
+    U(length(x), j) = -(j-11)^2+100;
 end
 
 % Define termination condition Ɛ (maximum error)
-epsilon = 0.01;
+epsilon = 1e-20;
 
 % Apply relexation method on all interior points until the residue r < Ɛ
 iterations = -1; % counter
@@ -35,7 +36,7 @@ while ~precise
         for j = 2 : length(y)-1
             
             % Calculate residue r
-            r = ( U(i+1,j) + U(i-1,j) + U(i,j+1) + U(i,j-1) - 4*U(i,j) ) / (4*h);
+            r = ( U(i+1,j) + U(i-1,j) + U(i,j+1) + U(i,j-1) - 4*U(i,j) ) / (4);
             U(i,j) = U(i,j) + r;
             
             if abs(r) >= epsilon
@@ -55,7 +56,7 @@ disp("Done after " + iterations + " iterations.");
 % Plotting
 [X,Y] = meshgrid(x,y);
 figure; surf(X,Y,U); % 3D
-figure; contour(X,Y,U); % from above
+xlabel('x'); ylabel('y'); zlabel('Potential in Volts');
 
-%subplot(2,2,3); contour3(X,Y,U) %shows a contour plot with colored lines xlabel('x'); ylabel('y'); zlabel('Potential in Volts');
-%subplot(2,2,4); meshc(X,Y,V) %shows a mesh plot of the solution xlabel('x'); ylabel('y'); zlabel('Potential in Volts');
+figure; contour(X,Y,U); % from above
+%figure; meshc(X,Y,U) %mesh: contour + surf
